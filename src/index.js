998adaf8a8ee47762aa38566aba19fc60da46f14
Guillaume Pitot <gpitot@nine.com.au>
2019-06-11 14:37:08 +1000

    no consoles

--- src/index.js
+++ src/index.js
@@ -20,25 +20,19 @@ function htmlLoaded() {
     //pass job , client, section
     //if none then dont track
     
-    console.log('iframe html loaded');
     console.log(window.frameElement);
     //setup ad scrolling
+    
     if (window.frameElement) {
-        console.log('iframe window frame element')
         //pass job , client, section
         const tracker = new Tracker({job:null, client:null, section:'fireplace'})
-        console.log('tracker created')
         const adscroller = new AdScrolling({iframe : window.frameElement});
         console.log(adscroller);
-        console.log('ad scroller created')
+        adscroller.setUpDimensions();
     }
 
     //images
     loadImages(parent, hostingLocation, () => {
-        console.log('iframe images loaded')
-
-        
-
         const dimensions = {
             rows : 3,
             cols : 12,
